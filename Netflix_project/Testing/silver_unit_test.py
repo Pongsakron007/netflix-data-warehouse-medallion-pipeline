25f2719f865148d7bb60710304ddd50aea1ed0f0
Pongsakron007 <pongsakronk007@gmail.com>
2026-08-19 09:06:26 +0000

    add comment in silver_unit_test
    add comment in silver_unit_test

--- Netflix_project/Testing/silver_unit_test.py
+++ Netflix_project/Testing/silver_unit_test.py
@@ -689,8 +689,7 @@ class TestSilverLayerWithMocks(unittest.TestCase):
         self.assertEqual(final_df.first()["show_id"], "s123")
 
     def test_something_else(self):
-        """Test something else. Like gold layer a bit further down the pipeline."""
-
+        """Test something else. Like gold layer a bit further down the pipeline. Should test something else"""
         pass
 
 
