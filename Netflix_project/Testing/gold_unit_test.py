e5c3327801240aa2decde5ca068de214b29567bc
pongsakronk009@hotmail.com <pongsakronk009@hotmail.com>
2026-09-08 13:32:29 +0000

    delete old code comment in testing file.
    delete old code comment in testing file.

--- Netflix_project/Testing/gold_unit_test.py
+++ Netflix_project/Testing/gold_unit_test.py
@@ -8,30 +8,6 @@ import os
 # 1. Add import to silver_unit_test.py
 from delta import configure_spark_with_delta_pip
 
-# # Import logic that works in both Databricks and GitHub runner
-# try:
-#     # Try GitHub package structure first
-#     from unified_fw.fw import GoldLayer
-# except ImportError:
-#     # Fallback for Databricks and local development
-#     try:
-#         # If __file__ is defined (GitHub runner, local Python)
-#         current_dir = os.path.dirname(os.path.abspath(__file__))
-#         project_root = os.path.dirname(current_dir)
-#         # Add logic_packages/src to path for unified_fw package
-#         package_path = os.path.join(project_root, 'logic_packages', 'src')
-#         if package_path not in sys.path:
-#             sys.path.insert(0, package_path)
-#     except NameError:
-#         # __file__ not defined (Databricks environment)
-#         # Use absolute path for Databricks - add the package source directory
-#         package_path = '/Workspace/Users/pongsakronk009@hotmail.com/netflix-data-warehouse-medallion-pipeline/Netflix_project/logic_packages/src'
-#         if package_path not in sys.path:
-#             sys.path.insert(0, package_path)
-    
-#     # Now import from unified_fw package
-#     from unified_fw.fw import GoldLayer
-
 # Import logic that works in GitHub Runner, Local, and Databricks
 try:
     from unified_fw.fw import GoldLayer
